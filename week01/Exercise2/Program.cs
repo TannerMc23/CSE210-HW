4e98d73cc40a7893ec1e1890122c0ac7010f3040
tannermc23 <tmcmillan@byupathway.edu>
2026-01-08 20:11:51 -0700

    Completed exercises.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is the grade percentage? ");
+        string percent = Console.ReadLine();
+
+        int number = int.Parse(percent);
+        string letter = "";
+
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is: {letter}.");
+
+        if (number >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console. WriteLine("You Failed!");
+        }
     }
 }
